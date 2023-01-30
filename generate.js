f275be1688c126e032f31770a710ad20d53b9e06
dandamudi2 <srinivasd2mail@gmail.com>
2023-01-31 01:09:11 +0800

    finalChanges

--- generate.js
+++ generate.js
@@ -1,27 +1,62 @@
-const btnGenerate = document.getElementById('btnGenerate');
+const btnGenerate = document.getElementById("btnGenerate");
 
-btnGenerate.addEventListener('click', function (e) {
+btnGenerate.addEventListener("click", function (e) {
+  e.preventDefault();
+  GeneratePwd();
+});
 
-    e.preventDefault();
-    GeneratePwd();
-   
+function GeneratePwd() {
+  const pwdLength = document.getElementById("lblLength").innerHTML;
+  let isChecked = false;
 
-})
+  let characters = "";
 
-function GeneratePwd(){
-    const pwdLength = document.getElementById('txtPwdLength').value;
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var pwd ='';
-    if(parseInt(pwdLength)>=100)
-    {
-        alert('Password length exceeding!');
-        return;
-    }
-    for(var i=0;i< parseInt(pwdLength);i++)
-    {
-        pwd += characters.charAt(Math.random() * characters.length);
-    }
+  if (document.getElementById("chkUpperCase").checked) {
+    isChecked = true;
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
 
-    document.getElementById('lblPassword').innerHTML = pwd;
+  if (document.getElementById("chkLowerCase").checked) {
+    isChecked = true;
+
+    characters += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (document.getElementById("chkNumbers").checked) {
+    isChecked = true;
+    characters += "0123456789";
+  }
+
+  if (document.getElementById("chkSymbols").checked) {
+    isChecked = true;
+    characters += "&$@=*";
+  }
+
+  if (isChecked === false) {
+    characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  }
+
+  var pwd = "";
+  if (parseInt(pwdLength) >= 100) {
+    alert("Password length exceeding!");
+    return;
+  }
+  document.getElementById("chkUpperCase").checked;
+  for (var i = 0; i < parseInt(pwdLength); i++) {
+    //if (document.getElementById("chkUpperCase").checked)
+
+    pwd += characters.charAt(Math.random() * characters.length);
+  }
+
+  document.getElementById("lblPassword").innerHTML = pwd;
 }
 
+document.getElementById("imgCopyToClipBoard").addEventListener("click", () => {
+  var lblClipBoard = document.getElementById("pwdCopiedToClipBoard");
+  lblClipBoard.innerHTML = "PasswordCopied To ClipBoard!";
+
+  setTimeout(() => {
+    lblClipBoard.innerHTML = "";
+  }, 2000);
+});
